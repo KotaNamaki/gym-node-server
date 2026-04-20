1b677a28acb8d5b234d4e44abbfb681c0effb5a5
Andhika Haqqa Syahrony <bban97550@gmail.com>
2026-04-21 01:46:58 +0700

    cors-helmet check

--- server.js
+++ server.js
@@ -30,7 +30,15 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
+app.use((req, res, next) => {
+    if ((req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') && !req.headers['content-type']) {
+        req.headers['content-type'] = 'application/json';
+    }
+    next();
+})
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Rate Limiting
 const authLimiter = rateLimit({
