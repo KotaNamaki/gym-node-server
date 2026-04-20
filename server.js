a02a53c88be09ebc234528a340b9bd97e6a9c5b0
Andhika Haqqa Syahrony <bban97550@gmail.com>
2026-04-21 01:29:27 +0700

    4th push

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ app.use('/api/views', viewsRoutes)  // ADD THIS - all view endpoints will be und
 
 app.use(errorMiddleware)
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const PORT = 3000
+app.listen(PORT, () => console.log(`Server running on localhost`))
